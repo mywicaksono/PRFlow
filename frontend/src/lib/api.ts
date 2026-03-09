f0d7502ef33bf0cf9b19d494c1c1b0785844fef6
itppm <itppmk0@gmail.com>
2026-03-09 14:19:06 +0700

    fix: configure API baseURL via VITE_API_BASE_URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+console.log('API BASE URL:', baseURL);
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: baseURL,
   headers: {
     Accept: 'application/json',
   },
